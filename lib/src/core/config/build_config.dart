20fd0991ddc6b03ad691e8c9d2602e3df1aeb917
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-08-23 06:50:09 +0600

    added flutter configs

---
+++ lib/src/core/config/build_config.dart
@@ -0,0 +1,24 @@
+import 'package:clean_architecture/src/core/config/config.dart';
+
+class BuildConfig {
+  late final EnvConfig envConfig;
+  late final Environment environment;
+  bool _isInstantiated = false;
+
+  static final BuildConfig instance = BuildConfig._internal();
+
+  BuildConfig._internal();
+
+  factory BuildConfig.instantiate({
+    required EnvConfig config,
+    required Environment type,
+  }) {
+    if (instance._isInstantiated) return instance;
+
+    instance.envConfig = config;
+    instance.environment = type;
+    instance._isInstantiated = true;
+
+    return instance;
+  }
+}
