f91e296b8ceb685ce83e18000330dd02014d1d53
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-08-23 08:18:52 +0600

    added app vaules

---
+++ lib/src/core/theme/app_values.dart
@@ -0,0 +1,80 @@
+abstract class AppValues {
+  static const double zero = 0;
+  static const double infinity = double.infinity;
+
+  static const double padding = 16;
+  static const double paddingSmall = 10;
+  static const double paddingXSmall = 8;
+  static const double paddingLarge = 24;
+  static const double paddingXLarge = 32;
+  static const double padding_6 = 6;
+  static const double padding_4 = 4;
+  static const double padding_2 = 2;
+  static const double padding_3 = 3;
+
+  static const double margin = 16;
+  static const double marginSmall = 8;
+  static const double marginXSmall = 6;
+  static const double marginLarge = 24;
+  static const double marginXarge = 36;
+  static const double margin_40 = 40;
+  static const double margin_32 = 32;
+  static const double margin_18 = 18;
+  static const double margin_2 = 2;
+  static const double margin_4 = 4;
+  static const double margin_6 = 6;
+  static const double margin_12 = 12;
+  static const double margin_10 = 10;
+  static const double margin_30 = 30;
+  static const double margin_20 = 20;
+  
+  static const double space = 96;
+
+  static const double radius = 16;
+  static const double radiusSmall = 8;
+  static const double radiusLarge = 24;
+  static const double radiusXLarge = 36;
+  static const double radius_6 = 6;
+  static const double radius_12 = 12;
+  static const double radiusRoundedButton = 24;
+
+  static const double elevationLvl0 = 0;
+  static const double elevationLvl1 = 1;
+  static const double elevationLvl2 = 3;
+  static const double elevationLvl3 = 6;
+  static const double elevationLvl4 = 10;
+  static const double elevationLvl5 = 16;
+
+  static const double circularImageDefaultSize = 90;
+  static const double circularImageSize_30 = 30;
+  static const int defaultAnimationDuration = 300;
+  static const double listBottomEmptySpace = 200;
+  static const double maxButtonWidth = 496;
+
+  static const double icon = 24;
+  static const double iconSmall = 16;
+  static const double iconXall = 12;
+  static const double iconLarge = 36;
+  static const double iconXLarger = 96;
+  static const double icon_20 = 20;
+  static const double icon_22 = 22;
+  static const double icon_18 = 18;
+  static const double icon_14 = 14;
+  static const double icon_28 = 28;
+  static const double appBarIcon = 32;
+
+  static const double customAppBarSize = 144.0;
+  static const double collapsedAppBarSize = 70.0;
+
+  static const int loggerLineLength = 120;
+  static const int loggerErrorMethodCount = 8;
+  static const int loggerMethodCount = 2;
+
+  static const double appbarActionRippleRadius = 50;
+  static const double barrierColorOpacity = 0.4;
+
+  static const int defaultDebounceTimeInMilliSeconds = 1000;
+  static const int defaultThrottleTimeInMilliSeconds = 500;
+
+  static const double height_16 = 16;
+}
