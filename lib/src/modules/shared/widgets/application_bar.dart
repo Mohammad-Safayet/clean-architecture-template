80d55c14d5a57d4bb3015283ea2b8bb4a9b65195
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-08-24 13:46:59 +0600

    added drawerIcon

--- lib/src/modules/shared/widgets/application_bar.dart
+++ lib/src/modules/shared/widgets/application_bar.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 
 import 'package:clean_architecture/src/modules/shared/widgets/app_bar_title.dart';
+import 'package:clean_architecture/src/core/theme/app_theme.dart';
 
 class ApplicationBar extends StatelessWidget implements PreferredSizeWidget {
   final String appBarTitleText;
   final List<Widget>? actions;
   final bool isBackButtonEnabled;
-  final Widget? drawer;
+  final Widget? drawerIcon;
 
   const ApplicationBar({
     Key? key,
     required this.appBarTitleText,
     this.actions,
     this.isBackButtonEnabled = true,
-    this.drawer,
+    this.drawerIcon,
   }) : super(key: key);
 
   @override
@@ -27,9 +28,11 @@ class ApplicationBar extends StatelessWidget implements PreferredSizeWidget {
       elevation: 0,
       automaticallyImplyLeading: isBackButtonEnabled,
       actions: actions,
-      // iconTheme: const IconThemeData(color: AppColors.appBarIconColor),
+      iconTheme: IconThemeData(
+        color: AppTheme.currentTheme.colorScheme.primary,
+      ),
       title: AppBarTitle(text: appBarTitleText),
-      leading: drawer,
+      leading: drawerIcon,
     );
   }
 }
