740d59f2c383c8bf08be4bdd8d0e81a099ce4096
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-09-03 08:14:46 +0600

    added app text_styles

---
+++ lib/src/core/theme/app_text_styles.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+abstract class AppTextStyles {
+  static const navigationBarLable = TextStyle(
+    height: 16,
+    fontSize: 12,
+    letterSpacing: 0.5,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const captions = TextStyle(
+    height: 16,
+    fontSize: 12,
+    letterSpacing: 0.5,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const title = TextStyle(
+    height: 24,
+    fontSize: 16,
+    letterSpacing: 0.15,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const titleBig = TextStyle(
+    height: 28,
+    fontSize: 24,
+    letterSpacing: 0.00,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const body = TextStyle(
+    height: 20,
+    fontSize: 14,
+    letterSpacing: 0.25,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const bodySmall = TextStyle(
+    height: 16,
+    fontSize: 12,
+    letterSpacing: 0.25,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const appBarTitleLarge = TextStyle(
+    height: 28,
+    fontSize: 22,
+    letterSpacing: 0.0,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const appBarTitle = TextStyle(
+    height: 24,
+    fontSize: 16,
+    letterSpacing: 0.15,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const appBarTitleSmall = TextStyle(
+    height: 20,
+    fontSize: 14,
+    letterSpacing: 0.1,
+    fontWeight: FontWeight.w500,
+  );
+}
