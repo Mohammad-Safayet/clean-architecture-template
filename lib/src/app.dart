32015c63b51bcb6124a95b016d93af45e1e3c9ab
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-09-03 08:31:20 +0600

    implemented router and locale

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,28 +1,39 @@
+import 'package:clean_architecture/src/core/routes/app_routes.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'package:clean_architecture/l10n/l10n.dart';
-import 'package:clean_architecture/src/modules/home/presenter/home_view.dart';
 import 'package:clean_architecture/src/core/theme/app_theme.dart';
+import 'package:clean_architecture/src/core/config/build_config.dart';
 
 
-class Application extends StatelessWidget {
-  const Application({super.key});
+class Application extends StatefulWidget {
+  const Application({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<Application> createState() => _ApplicationState();
+}
+
+class _ApplicationState extends State<Application> {
+  final config = BuildConfig.instance.envConfig;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return MaterialApp.router(
+      title: config.appName,
+      supportedLocales: L10n.ALL,
+      locale: L10n.currentLocale,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      supportedLocales: L10n.all,
-      locale: const Locale("en"),
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      themeMode: ThemeMode.dark,
-      home: HomeScreen(
-        appLocalizations: AppLocalizations.of(context)!,
-      ),
+      themeMode: ThemeMode.system,
+      routerConfig: AppRoutes.routes,
+      routerDelegate: AppRoutes.routes.routerDelegate,
+      routeInformationParser: AppRoutes.routes.routeInformationParser,
+      routeInformationProvider: AppRoutes.routes.routeInformationProvider,
     );
   }
 }
