448aa5c96d5782bbabcd67e2186ba86b35a2b4c1
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-09-07 06:51:52 +0600

    added new routes

--- lib/src/core/routes/app_routes.dart
+++ lib/src/core/routes/app_routes.dart
@@ -2,8 +2,14 @@ part of 'app_pages.dart';
 
 abstract class Routes {
   static const HOME = _Path.HOME;
+  static const COUNTER = _Path.COUNTER;
+  static const PRODUCTS = _Path.PRODUCTS;
+  static const PROFILE = _Path.PROFILE;
 }
 
 abstract class _Path {
   static const HOME = "/";
+  static const COUNTER = "/counter";
+  static const PRODUCTS = "/products";
+  static const PROFILE = "/profile";
 }
