0ccf28cd9872537ea004e8345926fd1a4146d96c
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-09-07 08:17:26 +0600

    changed product to store

--- lib/src/core/routes/app_routes.dart
+++ lib/src/core/routes/app_routes.dart
@@ -3,13 +3,13 @@ part of 'app_pages.dart';
 abstract class Routes {
   static const HOME = _Path.HOME;
   static const COUNTER = _Path.COUNTER;
-  static const PRODUCTS = _Path.PRODUCTS;
+  static const STORE = _Path.STORE;
   static const PROFILE = _Path.PROFILE;
 }
 
 abstract class _Path {
   static const HOME = "/";
   static const COUNTER = "/counter";
-  static const PRODUCTS = "/products";
+  static const STORE = "/store";
   static const PROFILE = "/profile";
 }
