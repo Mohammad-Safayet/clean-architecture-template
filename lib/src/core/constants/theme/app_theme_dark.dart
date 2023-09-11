d397c5d93a720a463e290bd18f6ed288231bfca3
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-09-11 13:57:39 +0600

    added app themes

---
+++ lib/src/core/constants/theme/app_theme_dark.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'package:clean_architecture/src/core/constants/app_color.dart';
+
+final appThemeDark = ThemeData(
+  colorScheme: AppColors.colorSchemeDark,
+  fontFamily: "Montserrat",
+  useMaterial3: true,
+  visualDensity: VisualDensity.adaptivePlatformDensity,
+);
+
---
+++ lib/src/core/constants/theme/app_theme_light.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'package:clean_architecture/src/core/constants/app_color.dart';
+
+final appThemeLight = ThemeData(
+  colorScheme: AppColors.colorSchemeLight,
+  fontFamily: "Montserrat",
+  useMaterial3: true,
+  visualDensity: VisualDensity.adaptivePlatformDensity,
+);
